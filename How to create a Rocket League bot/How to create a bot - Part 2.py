f53c52f27bf465f9e9fe0f06f854b3e40ce2b2e4
naturecodevoid <44983869+naturecodevoid@users.noreply.github.com>
2019-05-09 15:20:58 -0700

    Remove one un-needed line

--- How to create a Rocket League bot/How to create a bot - Part 2.py
+++ How to create a Rocket League bot/How to create a bot - Part 2.py
@@ -2,7 +2,6 @@ from rlbot.agents.base_agent import BaseAgent, SimpleControllerState
 from rlbot.utils.structures.game_data_struct import GameTickPacket
 import math
 
-
 class PythonExample(BaseAgent):
 
     def __init__(self, name, team, index):
