adc86eec3151c29729d8bae127082202885cf0ee
naturecodevoid <44983869+naturecodevoid@users.noreply.github.com>
2019-05-09 06:44:01 -0700

    Update How to create a bot - Part 1.py

--- How to create a Rocket League bot/How to create a bot - Part 1.py
+++ How to create a Rocket League bot/How to create a bot - Part 1.py
@@ -1,7 +1,8 @@
 from rlbot.agents.base_agent import BaseAgent, SimpleControllerState
 from rlbot.utils.structures.game_data_struct import GameTickPacket
 
-class TutorialBot(BaseAgent):
+class PythonExample(BaseAgent):
+
     def get_output(self, packet: GameTickPacket) -> SimpleControllerState:
         controller = SimpleControllerState()
         controller.throttle = 1
