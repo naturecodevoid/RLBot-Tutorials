fcb30f81be410318e6cc61cbcf2f2fc54930bb86
naturecodevoid <44983869+naturecodevoid@users.noreply.github.com>
2019-05-09 06:45:11 -0700

    Update How to create a bot - Part 5.py

--- How to create a Rocket League bot/How to create a bot - Part 5.py
+++ How to create a Rocket League bot/How to create a bot - Part 5.py
@@ -3,7 +3,8 @@ from rlbot.utils.structures.game_data_struct import GameTickPacket
 import math
 import time
 
-class TutorialBot(BaseAgent):
+class PythonExample(BaseAgent):
+
     def __init__(self, name, team, index):
         self.index = index
 
