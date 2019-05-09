4a1dc47a582e06bd16a27aeba528684131ce12d0
naturecodevoid <44983869+naturecodevoid@users.noreply.github.com>
2019-05-09 15:17:44 -0700

    Add init to part 1

--- How to create a Rocket League bot/How to create a bot - Part 1.py
+++ How to create a Rocket League bot/How to create a bot - Part 1.py
@@ -2,9 +2,12 @@ from rlbot.agents.base_agent import BaseAgent, SimpleControllerState
 from rlbot.utils.structures.game_data_struct import GameTickPacket
 
 class PythonExample(BaseAgent):
+    
+    def __init__(self, name, team, index):
+        super().__init__(name, team, index)
+        self.controller = SimpleControllerState()
 
     def get_output(self, packet: GameTickPacket) -> SimpleControllerState:
-        self.controller = SimpleControllerState()
         self.controller.throttle = 1
 
         return self.controller
